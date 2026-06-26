7c9d0a76d8a5ecac499286c53ce6988d9c3cd392
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-06-26 11:21:00 +0100

    feat: data state of the app in an array

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const posts = [
+  {
+    poster: {
+      name: "Vincent van Gogh",
+      location: "Zudert, Netherlands",
+      avatar: "/",
+      username: "vincey1853",
+    },
+    img: "/postImage.png",
+    text: "just took a few mushrooms lol",
+    likes: 21492,
+  },
+
+  {
+    poster: {
+      name: "Gustave Courbet",
+      location: "Ornans, France",
+      avatar: "/gustave-courbet.png",
+      username: "gus1819",
+    },
+    img: "/postImage1.png",
+    text: "i'm feelin a bit stressed tbh",
+    likes: 12502,
+  },
+
+  {
+    poster: {
+      name: "Joseph Ducreux",
+      location: "Paris, Francee",
+      avatar: "/joseph-ducreux.png",
+      username: "jd1735",
+    },
+    img: "/postImage2.png",
+    text: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes: 15137,
+  },
+];
+
+export { posts };
